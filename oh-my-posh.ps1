a4d4567f440bc681b244dfb601a8abc963dba973
Duncan Carpiso <delacruz.duncan@gmail.com>
2023-02-25 01:28:33 +0800

    Update oh-my-posh.ps1

--- oh-my-posh.ps1
+++ oh-my-posh.ps1
@@ -1,1 +1,6 @@
+Install-Module PowerShellGet -AllowClobber -Force; 
+Install-Module -Name PSReadLine -AllowPrerelease; 
+Install-Module -Name PowerShellGet -Force; 
+Install-Module PSReadLine; 
+Install-Module -Name Terminal-Icons -Repository PSGallery; 
 Start-Process winget -Verb runas -ArgumentList "install", "JanDeDobbeleer.OhMyPosh"
