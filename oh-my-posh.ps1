05b6c2722812e0d2966b5491b217f6f588222649
Duncan Carpiso <delacruz.duncan@gmail.com>
2023-02-25 01:06:05 +0800

    Update oh-my-posh.ps1

--- oh-my-posh.ps1
+++ oh-my-posh.ps1
@@ -1,1 +1,1 @@
-start-process PowerShell -verb runas || winget install JanDeDobbeleer.OhMyPosh || New-Item -Path $PROFILE -Type File -Force
+start-process PowerShell -verb runas || winget install JanDeDobbeleer.OhMyPosh
